68b17e2e9d5668776b201dad48ff0211b1aed8af
David George Holmes <geometryzen@gmail.com>
2024-06-18 10:45:16 -0400

    standalone

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ git status
 git add --all
 echo "Please enter a commit message"
 read message
-git commit -m "'$message'"
+git commit -m "$message"
 git push origin main
 npm run release
 npm run docs
